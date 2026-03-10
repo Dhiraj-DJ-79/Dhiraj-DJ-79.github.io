0aee5194606ccfb8c21d7c170259abc6a768ba9e
Dhiraj-DJ-79 <dhirajsaw7979@gmail.com>
2026-03-10 18:04:11 +0000

    Deploying to gh-pages from @ Dhiraj-DJ-79/Dhiraj-DJ-79.github.io@33b1bce3a7c1a6bba9cfefa342d1c5649c82c65a 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,183 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "Research initiatives, pre-prints, and works in progress.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "Research initiatives and academic projects in Stochastic Calculus and SciML.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-bookshelf",
+          title: "bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Courses assisted during my doctoral studies at IIT Goa.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-people",
+          title: "people",
+          description: "Members of the research group.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
+        },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/books/";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/blog/";
+              },
+            },{id: "post-why-we-need-em-the-sum-inside-the-log-and-the-dream-of-complete-data",
+        
+          title: "Why We Need EM: The Sum Inside the Log and the Dream of...",
+        
+        description: "When standard MLE fails for Gaussian Mixture Models, Jensen&#39;s Inequality and the EM algorithm step in to save the day by &#39;completing&#39; our data.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/why-we-need-em/";
+          
+        },
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "projects-pinns-and-feynman-kac-for-high-dimensional-pdes",
+          title: 'PINNs and Feynman-Kac for High-Dimensional PDEs',
+          description: "Solving high-dimensional heat equations using a hybrid of Stochastic Calculus and Deep Learning.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-inverse-elliptic-pdes-via-dtn-mapping",
+          title: 'Inverse Elliptic PDEs via DtN Mapping',
+          description: "A Machine Learning and Finite Element framework for solving inverse problems.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-manual-optimizers-vs-svms-on-mnist",
+          title: 'Manual Optimizers vs. SVMs on MNIST',
+          description: "A comparative study of optimization algorithms in classical and modern Machine Learning.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-fourth-order-scheme-for-navier-stokes",
+          title: 'Fourth-Order Scheme for Navier-Stokes',
+          description: "High-order finite difference schemes for steady incompressible flow.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/cv.pdf", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/dhiraj-saw-4889a4377", "_blank");
+        },
+      },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%64%68%69%72%61%6A%32%34%32%33%32%32%30%33@%69%69%74%67%6F%61.%61%63.%69%6E", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=Kyr2ResAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Dhiraj-DJ-79", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
